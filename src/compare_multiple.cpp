e0ab13335e25a9ddc1a1b3679628e816068a9509
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2022-01-11 16:29:45 +0200

    Save the completeness as a percentage

--- src/compare_multiple.cpp
+++ src/compare_multiple.cpp
@@ -137,7 +137,7 @@ int main(int argc, char** argv)
         meshDifference.setSourceMesh(sourceMeshData.mesh);
         meshDifference.setTargetMesh(targetMeshDataIt->mesh);
         const float accuracy = meshDifference.computeDifference();
-        const float completeness = meshDifference.computeCompleteness(inlierThreshold);
+        const float completeness = 100.0f * meshDifference.computeCompleteness(inlierThreshold);
         const float desired_scale =
             percentage_at_scale(extract_mesh_scales(sourceMeshData.filename.string()), 0);
 
